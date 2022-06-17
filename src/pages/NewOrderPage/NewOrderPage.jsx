3597d1ed10504a6c8901c3fcf30cd5837ea51927
Hemalatha-kola <kola123hema@gmail.com>
2022-06-17 10:27:17 -0700

    created items-api

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -6,7 +6,11 @@ export default function NewOrderPage(){
 
     useEffect(function() {
         console.log('NewOrderPage rendered');
-      });
+    });
+
+    useEffect(function() {
+        console.log('useEffect runs only after first render');
+    }, []);
     return (
         <h1>NewOrderPage</h1>
     );
