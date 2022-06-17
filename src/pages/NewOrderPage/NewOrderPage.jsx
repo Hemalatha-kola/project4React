b55b46a3393860958fded1421ee94b9b25b706dc
Hemalatha-kola <kola123hema@gmail.com>
2022-06-17 10:24:40 -0700

    imported useEffect

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function NewOrderPage(){
     const [menuItems, setMenuItems] = useState([]);
+
+    useEffect(function() {
+        console.log('NewOrderPage rendered');
+      });
     return (
         <h1>NewOrderPage</h1>
     );
