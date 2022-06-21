e8c089ccc56e378a571e727c5ff600522ffca1b8
Hemalatha-kola <kola123hema@gmail.com>
2022-06-20 20:12:18 -0700

    background image added to home and new page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,30 @@
+import "./About.css";
+
 export default function (){
     return(
-        <>
+       <>
+        <div className="about">
         <h1>About</h1>
         <p>Best place to Know Top Rated Restaurants in Sacramento</p>
         <div className="home-page-div">
-        <div><img className="home-page-img" src="https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /></div>
-        <div><img className="home-page-img" src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /></div>
+        <div><img className="home-page-img" src="https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" /></div>
+        <div><img className="home-page-img" src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/></div>
         
-        <div><img className="home-page-img" src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM=" /></div>
+        <div><img className="home-page-img" src="https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240?b=1&k=20&m=1295387240&s=170667a&w=0&h=KtWYFjSBgpNgVkE3P-WKZ2F6V-VxyUBBtJIP_k8IANM=" alt=""/></div>
+        <div><img className="home-page-img" src="https://media.cntraveler.com/photos/5b22bfea9a9e466ba59f567a/master/w_4000,h_2857,c_limit/Pukka_Catherine-Hendry_2018_overhead-hands.jpg" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://media-cdn.tripadvisor.com/media/photo-s/17/43/00/e7/mix-great-combination.jpg" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://www.sacbee.com/latest-news/3ibmvs/picture230673654/alternates/FREE_1140/LEDE_SAC_20190521_AB_Fats" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://s3-media0.fl.yelpcdn.com/bphoto/kU8JkIDeiiCxM-yflMqkhA/348s.jpg" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://mikunisushi.com/wp-content/uploads/2013/12/midtown2.jpg" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://kitchensacramento.com/wp-content/uploads/2020/08/1-2.jpg" alt=""/></div>
+        
+        <div><img className="home-page-img" src="https://assets-global.website-files.com/5a5ecb0dd9432b0001290351/60ec75fea237823986ab94ec_dsc08385-2.JPG" alt=""/></div>
+        </div>
         </div>
         </>
     )
